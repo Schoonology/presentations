0ddd30bd3a794e4a8d106d501aefef4acac32fba
Schoonology <michael.r.schoonmaker@gmail.com>
2017-03-18 11:10:01 -0700

    Add shared assets.

---
+++ shared/style.js
@@ -0,0 +1,36 @@
+import createTheme from "spectacle/lib/themes/default"
+
+var backgroundColors = ['primary-bg', 'secondary-bg', 'tertiary-bg']
+var keys = []
+function getBackground(key) {
+  var index = keys.indexOf(key)
+
+  if (index === -1) {
+    index = keys.push(key) - 1
+  }
+
+  return backgroundColors[index % backgroundColors.length]
+}
+
+export const colors = {
+  PRIMARY_BG: 'primary-bg',
+  SECONDARY_BG: 'secondary-bg',
+  TERTIARY_BG: 'tertiary-bg',
+  BRAND_TEXT: 'green',
+  PRIMARY_TEXT: 'white',
+  SUBTLE_TEXT: 'grey',
+  bg: getBackground,
+}
+
+export const theme = createTheme({
+  'primary-bg': '#404040',
+  'secondary-bg': '#336699',
+  'tertiary-bg': '#e73c71',
+  'green': '#6eff00',
+  'white': '#f8f8f8',
+  'grey': '#909090',
+}, {
+  primary: 'Source Sans Pro',
+})
+
+console.log(theme)
